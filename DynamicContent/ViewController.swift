dc01a93e6ed94207a609e47b0de64b5985a1eaff
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2023-05-19 13:33:30 +0800

    Initial Commit

---
+++ DynamicContent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicContent
+//
+//  Created by Kolyutsakul, Thongchai on 19/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
